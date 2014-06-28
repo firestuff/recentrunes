5fc694319e7daf9c8ab9210eeb07aa6729837922
Ian Gulliver <ian@isinglasstheater.org>
2014-06-27 23:07:33 -0700

    Passing test.

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
-asyncTest('Simple', function() {
+QUnit.test('Simple', function(assert) {
+  assert.expect(1);
   var context = new rr.Context(mediawiki, 
 "=== Heading ===\n\
 This is a wiki doc.\n\
 How about some '''bold and <i>bold italic</i>'''.\n\
 I would also love some <nowiki>nowiki <b>foo</b></nowiki>");
   var iterable = context.rules['wikidoc'].match(context);
-  console.log(iterable.next());
+  assert.equal(iterable.next().value.nodes[0].innerHTML,
+"<h3>Heading</h3>This is a wiki doc.\n\
+How about some <b>bold and &lt;i&gt;bold italic&lt;/i&gt;</b>.\n\
+I would also love some nowiki &lt;b&gt;foo&lt;/b&gt;");
 });
